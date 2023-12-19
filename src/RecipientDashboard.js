221d5a87fe556c0cad6ad06b6a2937c3c4c0a445
codesbyUmair <i211182@nu.edu.pk>
2023-12-19 15:10:47 +0500

    show ,edit and delete doctors

--- src/RecipientDashboard.js
+++ src/RecipientDashboard.js
@@ -1,77 +1,197 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const AddDoctor = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    const [error, setError] = useState('');
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
-        const response = await fetch('http://localhost:3030/admin/add', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            email,
-            password,
-            role: 'doctor',
-          }),
-        });
-  
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-  
-        const data = await response.json();
-  
-        if (data.message) {
-          setError(data.message);
-        } else {
-          console.log('Doctor added successfully:', data);
-          setEmail('');
-          setPassword('');
-          navigate('/recipient');
-        }
-      } catch (error) {
-        console.error('Error adding Doctor:', error.message);
+const RecipientDashboard = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [doctors, setDoctors] = useState([]);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3030/recipient/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+          role: 'doctor',
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    };
-  
-    return (
-      <div>
-        <h2>Add Doctor</h2>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <form onSubmit={handleSubmit}>
-          <label>
-            Doctor Email:
-            <input
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
-          <br />
-          <label>
-            Doctor Password:
-            <input
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </label>
-          <br />
 
-          <button type="submit">Add Doctor</button>
-        </form>
-      </div>
-    );
+      const data = await response.json();
+
+      if (data.message) {
+        setError(data.message);
+      } else {
+        console.log('Doctor added successfully:', data);
+        setName('');
+        setEmail('');
+        setPassword('');
+        handleGetAllDoctors(); // Refresh the doctor list after adding a new one
+      }
+    } catch (error) {
+      console.error('Error adding Doctor:', error.message);
+    }
   };
-  
-  export default AddDoctor;
-  
-  
+
+  const handleGetAllDoctors = async () => {
+    try {
+      const response = await fetch('http://localhost:3030/recipient/getAll');
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setDoctors(data);
+    } catch (error) {
+      console.error('Error getting doctors:', error.message);
+    }
+  };
+
+  const handleUpdateDoctor = (doctor) => {
+    // Set the selected doctor and show the update modal
+    setSelectedDoctor(doctor);
+    setShowUpdateModal(true);
+  };
+
+  const handleUpdateSubmit = async () => {
+    try {
+      // Implementation for updating the selected doctor (similar to your existing code)
+      console.log('Updating doctor:', selectedDoctor._id);
+      setShowUpdateModal(false);
+    } catch (error) {
+      console.error('Error updating doctor:', error.message);
+    }
+  };
+
+  const handleDeleteDoctor = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3030/recipient/delete/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      console.log('Doctor deleted successfully');
+      handleGetAllDoctors(); // Refresh the doctor list after deleting one
+    } catch (error) {
+      console.error('Error deleting doctor:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    handleGetAllDoctors();
+  }, []);
+
+  return (
+    <div className="doctor-container">
+      <h2>Add Doctor</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleSubmit} className="doctor-form">
+        <label>
+          Doctor Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Doctor Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Doctor Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <button type="submit">Add Doctor</button>
+      </form>
+
+      {showUpdateModal && selectedDoctor && (
+        <div className="update-modal">
+          <h2>Update Doctor</h2>
+          <form onSubmit={handleUpdateSubmit} className="doctor-form">
+            <label>
+              Doctor Name:
+              <input
+                type="text"
+                value={selectedDoctor.name}
+                onChange={(e) => setSelectedDoctor({ ...selectedDoctor, name: e.target.value })}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Doctor Email:
+              <input
+                type="email"
+                value={selectedDoctor.email}
+                onChange={(e) => setSelectedDoctor({ ...selectedDoctor, email: e.target.value })}
+                required
+              />
+            </label>
+            <br />
+            <label>
+              Doctor Password:
+              <input
+                type="password"
+                value={selectedDoctor.password}
+                onChange={(e) => setSelectedDoctor({ ...selectedDoctor, password: e.target.value })}
+                required
+              />
+            </label>
+            <br />
+            <button type="submit">Update Doctor</button>
+          </form>
+        </div>
+      )}
+
+      {/* Display All Doctors */}
+      <div>
+        <h2>All Doctors</h2>
+        <ul>
+          {doctors.map((doctor) => (
+            <li key={doctor._id}>
+              {doctor.name} - {doctor.email}
+              <button onClick={() => handleUpdateDoctor(doctor)}>Update</button>
+              <button onClick={() => handleDeleteDoctor(doctor._id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default RecipientDashboard;
