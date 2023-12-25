28931a55c1e7a41bdfabd775ea4017b41c24e65b
codesbyUmair <i211182@nu.edu.pk>
2023-12-25 13:34:35 +0500

    get appointment

--- src/RecipientDashboard.js
+++ src/RecipientDashboard.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate,Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const RecipientDashboard = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [availableDays, setAvailableDays] = useState('');
-  const [availableTimeStart, setAvailableTimeStart] = useState('');
-  const [availableTimeEnd, setAvailableTimeEnd] = useState('');
   const [availableDays, setAvailableDays] = useState('');
   const [availableTimeStart, setAvailableTimeStart] = useState('');
   const [availableTimeEnd, setAvailableTimeEnd] = useState('');
@@ -34,9 +31,6 @@ const RecipientDashboard = () => {
           availableDays,
           availableTimeStart,
           availableTimeEnd,
-          availableDays,
-          availableTimeStart,
-          availableTimeEnd,
         }),
       });
 
@@ -53,9 +47,6 @@ const RecipientDashboard = () => {
         setName('');
         setEmail('');
         setPassword('');
-        setAvailableDays('');
-        setAvailableTimeStart('');
-        setAvailableTimeEnd('');
         setAvailableDays('');
         setAvailableTimeStart('');
         setAvailableTimeEnd('');
@@ -82,39 +73,12 @@ const RecipientDashboard = () => {
   };
 
   const handleUpdateDoctor = (doctor) => {
+    // Set the selected doctor and show the update modal
     setSelectedDoctor(doctor);
     setShowUpdateModal(true);
   };
 
   const handleUpdateSubmit = async () => {
-  try {
-    const response = await fetch(`http://localhost:3030/recipient/update/${selectedDoctor._id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: selectedDoctor.name,
-        email: selectedDoctor.email,
-        password: selectedDoctor.password,
-        availableDays: selectedDoctor.availableDays,
-        availableTimeStart: selectedDoctor.availableTimeStart,
-        availableTimeEnd: selectedDoctor.availableTimeEnd,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    console.log('Doctor updated successfully:', selectedDoctor._id);
-    setShowUpdateModal(false);
-    handleGetAllDoctors();
-  } catch (error) {
-    console.error('Error updating doctor:', error.message);
-  }
-};
-
   try {
     const response = await fetch(`http://localhost:3030/recipient/update/${selectedDoctor._id}`, {
       method: 'PUT',
@@ -155,7 +119,7 @@ const RecipientDashboard = () => {
       }
 
       console.log('Doctor deleted successfully');
-      handleGetAllDoctors(); 
+      handleGetAllDoctors(); // Refresh the doctor list after deleting one
     } catch (error) {
       console.error('Error deleting doctor:', error.message);
     }
@@ -208,36 +172,6 @@ const RecipientDashboard = () => {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Available Days:
-          <input
-            type="text"
-            value={availableDays}
-            onChange={(e) => setAvailableDays(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Available Start Time:
-          <input
-            type="text"
-            value={availableTimeStart}
-            onChange={(e) => setAvailableTimeStart(e.target.value)}
-            required
-          />
-        </label>
-        <br />
-        <label>
-          Available End Time:
-          <input
-            type="text"
-            value={availableTimeEnd}
-            onChange={(e) => setAvailableTimeEnd(e.target.value)}
             required
           />
         </label>
@@ -343,77 +277,9 @@ const RecipientDashboard = () => {
     </form>
   </div>
 )}
-  <div className="update-modal">
-    <h2>Update Doctor</h2>
-    <form onSubmit={handleUpdateSubmit} className="doctor-form">
-      <label>
-        Doctor Name:
-        <input
-          type="text"
-          value={selectedDoctor.name}
-          onChange={(e) => setSelectedDoctor({ ...selectedDoctor, name: e.target.value })}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Doctor Email:
-        <input
-          type="email"
-          value={selectedDoctor.email}
-          onChange={(e) => setSelectedDoctor({ ...selectedDoctor, email: e.target.value })}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Doctor Password:
-        <input
-          type="password"
-          value={selectedDoctor.password}
-          onChange={(e) => setSelectedDoctor({ ...selectedDoctor, password: e.target.value })}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Available Days:
-        <input
-          type="text"
-          value={selectedDoctor.availableDays}
-          onChange={(e) => setSelectedDoctor({ ...selectedDoctor, availableDays: e.target.value })}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Available Start Time:
-        <input
-          type="text"
-          value={selectedDoctor.availableTimeStart}
-          onChange={(e) => setSelectedDoctor({ ...selectedDoctor, availableTimeStart: e.target.value })}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Available End Time:
-        <input
-          type="text"
-          value={selectedDoctor.availableTimeEnd}
-          onChange={(e) => setSelectedDoctor({ ...selectedDoctor, availableTimeEnd: e.target.value })}
-          required
-        />
-      </label>
-      <br />
-      <button type="submit">Update Doctor</button>
-    </form>
-  </div>
-)}
 
 
-
-
+      {/* Display All Doctors */}
       <div>
         <h2>All Doctors</h2>
         <ul>
@@ -427,9 +293,9 @@ const RecipientDashboard = () => {
         </ul>
       </div>
     </div>
-  </div>
+    </div>
+    
   );
 };
 
 export default RecipientDashboard;
-
